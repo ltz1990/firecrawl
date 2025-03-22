4a598a6602ecef1d5c9032b02198025f406fc3d7
libai <libai@gmail.com>
2025-03-22 14:14:44 +0800

    update

--- apps/api/src/scraper/scrapeURL/index.ts
+++ apps/api/src/scraper/scrapeURL/index.ts
@@ -215,6 +215,7 @@ function safeguardCircularError<T>(error: T): T {
 
 async function scrapeURLLoop(meta: Meta): Promise<ScrapeUrlResponse> {
   meta.logger.info(`Scraping URL ${JSON.stringify(meta.url)}...`);
+  meta.logger.info("测试3", meta);
 
   // TODO: handle sitemap data, see WebScraper/index.ts:280
   // TODO: ScrapeEvents
